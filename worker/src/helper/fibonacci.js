f8e52b584ffca01f4526e16ebb9186cf21bae773
Retr0327 <philcoke35@gmail.com>
2022-03-04 10:33:33 +0800

    Update fibonacci

--- worker/src/helper/fibonacci.js
+++ worker/src/helper/fibonacci.js
@@ -1,9 +1,9 @@
 function fibonacci(index) {
   if (index < 2) {
     return 1;
-  } 
+  }
 
-  return fibonacci(index - 1) + fibonacci(fibonacci - 2);
+  return fibonacci(index - 1) + fibonacci(index - 2);
 }
 
 export default fibonacci;

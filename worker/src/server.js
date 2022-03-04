614c3e2c92d7b8992e826f34f41ba9b7e6ee8bd8
Retr0327 <philcoke35@gmail.com>
2022-03-04 10:33:12 +0800

    Update server

--- worker/src/server.js
+++ worker/src/server.js
@@ -1,30 +1,5 @@
-// import Redis from "ioredis";
-// import keys from "./constants/keys.js";
-
-// function fib(index) {
-//   if (index < 2) return 1;
-//   return fib(index - 1) + fib(index - 2);
-// }
-
-// let { redisHost, redisPort } = keys;
-
-// const redis = new Redis(`redis://${redisHost}:${redisPort}`);
-// const redisSub = new Redis(`redis://${redisHost}:${redisPort}`);
-
-// redisSub.subscribe("insert", (err, count) => {
-//   if (err) {
-//     console.error("Failed to subscribe: %s", err.message);
-//   }
-//   console.log(`subscribed to ${count} channels.`);
-// });
-
-// redisSub.on("message", (channel, message) => {
-//   console.log(`Received ${message} from ${channel}`);
-//   redis.hset("values", message, fib(parseInt(message)));
-// });
-
 import fibonacci from "./helper/fibonacci.js";
-import redisSubscriber from "./model/redisSubscriber.js";
+import { redisCli, redisSubscriber } from "./model/index.js";
 
 redisSubscriber.subscribe("insert", (err, count) => {
   if (err) {
@@ -35,5 +10,5 @@ redisSubscriber.subscribe("insert", (err, count) => {
 
 redisSubscriber.on("message", (channel, message) => {
   console.log(`Received ${message} from ${channel}`);
-  redis.hset("values", message, fibonacci(parseInt(message)));
+  redisCli.hset("values", message, fibonacci(parseInt(message)));
 });

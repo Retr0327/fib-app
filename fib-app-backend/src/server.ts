0b4921d757bee8dacb11806888048dacb9f14a46
Retr0327 <philcoke35@gmail.com>
2022-05-15 20:12:59 +0800

    Update server

--- fib-app-backend/src/server.ts
+++ fib-app-backend/src/server.ts
@@ -1,14 +1,9 @@
 import app from "./api/app";
-import { dockerEnv } from "./api/constants";
 
-const { port } = dockerEnv;
-
-const PORT = port || 3000;
+const PORT = process.env.PORT || 3000;
 
 const server = app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
 
 export default server;
-
-    

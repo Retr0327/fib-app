0d8e595d5c840ca27ce2c43a72735956fb79e61b
Retr0327 <philcoke35@gmail.com>
2022-05-15 20:13:04 +0800

    Add valueRoute

--- fib-app-backend/src/api/app.ts
+++ fib-app-backend/src/api/app.ts
@@ -1,13 +1,16 @@
 import Koa from "koa";
 import cors from "@koa/cors";
-import corsConfig from "./helpers/corsConfig"
-import { rootRoute } from "./routes";
+import koaBody from "koa-body";
+import { corsConfig } from "./helpers";
+import { rootRoute, valueRoute } from "./routes";
 
 const app = new Koa();
 app.use(cors(corsConfig));
+app.use(koaBody({ urlencoded: true }));
 
 app.use(rootRoute.routes());
 app.use(rootRoute.allowedMethods());
+app.use(valueRoute.routes());
+app.use(valueRoute.allowedMethods());
 
 export default app;
-    

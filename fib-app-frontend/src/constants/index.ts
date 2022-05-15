6bc43fca2a01d5b4559356b9ee8cb5999528d5e6
Retr0327 <philcoke35@gmail.com>
2022-05-15 23:31:08 +0800

    Add index

---
+++ fib-app-frontend/src/constants/index.ts
@@ -0,0 +1,9 @@
+const isProduction: boolean = process.env.NODE_ENV === "production";
+
+const API: string = !isProduction ? "http://localhost:3000" : "/api";
+
+const EXTERNAL_API: string = !isProduction
+  ? "http://localhost:3000"
+  : `http://api:${process.env.API_PORT}`;
+
+export { API, EXTERNAL_API };

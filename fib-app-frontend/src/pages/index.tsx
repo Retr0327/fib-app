6f8a7630297b18c70bd181764735f2f337e54aba
Retr0327 <philcoke35@gmail.com>
2022-05-16 15:45:00 +0800

    Update index

--- fib-app-frontend/src/pages/index.tsx
+++ fib-app-frontend/src/pages/index.tsx
@@ -1,8 +1,24 @@
-import type { NextPage } from "next";
+import { FibPagePropsType } from "types";
 import FibPage from "@containers/FibPage";
+import type { GetServerSideProps } from "next";
+import { fetchAllIndexes, fetchCurrentValues } from "src/services";
 
-const Home: NextPage = () => {
-  return <FibPage />;
-};
+function Home({ allValues, currentIndex }: FibPagePropsType) {
+  return <FibPage currentIndex={currentIndex} allValues={allValues} />;
+}
 
 export default Home;
+
+export const getServerSideProps: GetServerSideProps<FibPagePropsType> = async (
+  context
+) => {
+  const [allIndexesResult, indexError] = await fetchAllIndexes();
+  const [currentValueResult, currentValueError] = await fetchCurrentValues();
+
+  return {
+    props: {
+      allValues: allIndexesResult.data,
+      currentIndex: currentValueResult.data,
+    },
+  };
+};

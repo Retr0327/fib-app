2b5863e8039cc1bdd3de3cf53b013f5654dc825d
Retr0327 <philcoke35@gmail.com>
2022-05-16 18:03:58 +0800

    Update fibonacci

--- worker/src/api/helper/fibonacci.ts
+++ worker/src/api/helper/fibonacci.ts
@@ -1,9 +1,15 @@
 function fibonacci(index: number): number {
-  if (index < 2) {
+  if (index === 0) {
+    return 0;
+  }
+
+  if (index === 1) {
     return 1;
   }
 
-  return fibonacci(index - 1) + fibonacci(index - 2);
+  return fibonacci(index - 2) + fibonacci(index - 1);
 }
 
 export default fibonacci;
+
+console.log(fibonacci(6));

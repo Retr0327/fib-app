8512d3afde796a2f981b5ceff0612b178d391bd2
Retr0327 <philcoke35@gmail.com>
2022-05-16 18:28:40 +0800

    Update FibPageForm

--- fib-app-frontend/src/containers/FibPageForm.tsx
+++ fib-app-frontend/src/containers/FibPageForm.tsx
@@ -3,6 +3,10 @@ import { Grid, Box, Button } from "@mui/material";
 import { Formik, Form, FormikHelpers } from "formik";
 import { FibPagePropsType, FormValueType } from "types";
 import FormikController from "@components/Form/FormikController";
+import { IndexBoard } from "@components/index";
+
+
+//  currentIndex [ { index: '4', fib: '3' }, { index: '6', fib: '8' } ]
 
 function FibPageForm({ allValues, currentIndex }: FibPagePropsType) {
   const initialValues: FormValueType = {
@@ -37,24 +41,30 @@ function FibPageForm({ allValues, currentIndex }: FibPagePropsType) {
               width={300}
               sx={{ justifyContent: "center", alignItems: "center" }}
             >
-              <Box
-                sx={{
-                  display: "flex",
-                  flexDirection: "row",
-                  flexWrap: "nowrap",
-                  gap: 3,
-                  width: "100%",
-                }}
-              >
-                <FormikController
-                  control="text-input"
-                  name="inputIndex"
-                  label="Fib index"
-                />
-                <Button sx={{ height: 55 }} type="submit" variant="contained">
-                  新增
-                </Button>
-              </Box>
+              <Grid item xs={12} md={12}>
+                <Box
+                  sx={{
+                    display: "flex",
+                    flexDirection: "row",
+                    flexWrap: "nowrap",
+                    gap: 3,
+                    width: "100%",
+                  }}
+                >
+                  <FormikController
+                    control="text-input"
+                    name="inputIndex"
+                    label="Fib index"
+                  />
+                  <Button sx={{ height: 55 }} type="submit" variant="contained">
+                    新增
+                  </Button>
+                </Box>
+              </Grid>
+
+              <Grid item xs={12} md={12}>
+                <IndexBoard allValues={allValues} />
+              </Grid>
             </Grid>
           </Form>
         );
